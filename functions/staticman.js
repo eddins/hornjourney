f3d4389ee4e26feeb0d49251a5bcbff4cf81c392
eddins <4043689+eddins@users.noreply.github.com>
2024-01-14 14:55:34 -0500

    Tweak staticman.js

--- functions/staticman.js
+++ functions/staticman.js
@@ -28,7 +28,7 @@ exports.handler = (event, context, callback) => {
           },
         },
         moderation: true,
-        path: "data/comments",
+        path: "_data/comments/{options.slug}",
         requiredFields: ["name", "message", "post"],
       },
     },
